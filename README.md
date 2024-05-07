938f259a750161314c608b915aa3c39359fc69f9
Tyler Byrd <tyler.byrd@cvent.com>
2024-05-07 11:15:18 -0400

    Updated the ReadMe File

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# outdoorsproject
+# Outdoors Project
+
+## Project Description
+The "Enjoy the Outdoors" Project allows users to view information about the National Parks and Mountains in the United States. The National Parks Search Page enables users to search for national parks by state/territory or park type. The Mountains Information Page features a dropdown menu which allows users to view information about 48 mountains.
+
+## Webpage Images
+
+![Homepage Screenshot.](/image/homepage.jpg "This is a screenshot of the homepage.")
+
+![National Parks Screenshot.](/image/national-parks.jpg "This is a screenshot of the National Parks page.")
+
+![Mountains Screenshot.](/image/mountains.jpg "This is a screenshot of the Mountains page.")
+
+## Code Sample
+![Code Sample.](/image/code-sample.jpg "This is a sample of my code.")
+The provided code sample is from the National Parks page. The displayParkType function obtains a reference to the HTML table element, allowing for data manipulation.  The function filters the National Park data stored in the nationalParksArray. To apply the filter, it standardizes the park types selected from the dropdown menu and the corresponding location names from the nationalParksArray by converting them to lowercase.  Then, it checks if each location name in the nationalParksArray contains the park type selected by the user. Finally, the function clears the table's contents in preparation for displaying the updated data.
--- scripts/mountains.js
+++ scripts/mountains.js
@@ -17,45 +17,31 @@ window.onload = function () {
         }
         dropdown.addEventListener("click", displayData);
 }
-//Function to Display the Data
+
 function displayData(event) {
     document.querySelector(".container").style.display = "block";
-    //Select the Card 
     let card = document.getElementById("card");
     let selectedElement = event.target.getAttribute("data-value");
-    //Loop through the data and check if the value is equal to the selected item
     let filteredData = mountainsArray.filter(item => item.name === selectedElement);
    
     filteredData.forEach(item => {
-        //TITLE
-        //Select the paragraph element
         let paragraph = document.getElementById("card-title");
-        //Change the Content of the paragraph element
         let description = item.desc;
 
         paragraph.textContent = item.name;
 
-        //DESCRIPTION
-        //Get the Description Element
         let element = document.getElementById("description");
-
-        //Define the New Text
         let newText = description;
 
-        //Set the Text to the Element
         element.textContent = newText;
 
-        //ELEVATION
         let elevation = document.getElementById("elevation");
         let elevationText = "Elevation: " + item.elevation + " Feet";
         elevation.textContent = elevationText;
 
-        //IMAGE
         let image = document.getElementById("image");
         image.src = "images/" + item.img;        
 
-
-    // function that can "fetch" the sunrise/sunset times
     async function getSunsetForMountain(lat, lng){
     let response = await fetch(
     `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
@@ -64,21 +50,17 @@ function displayData(event) {
     return data;
 }
 
-    // Fetch the sunset/sunrise times for a specific mountain
     getSunsetForMountain(item.lat, item.lng).then(data => {
-        // Formatting options for time only
         const options = {
-            hour: 'numeric', // '2-digit', 'numeric'
-            minute: '2-digit', // 'numeric'
-            hour12: true // to display in 12-hour format
+            hour: 'numeric',
+            minute: '2-digit', 
+            hour12: true 
         };
 
-        //Sunrise
         let sunrise = document.getElementById("sunrise");
         let sunriseText = "Sunrise: " + data.results.sunrise;
         sunrise.textContent = sunriseText;
 
-        //Sunrise
         let sunset = document.getElementById("sunset");
         let sunsetText = "Sunset: " + (data.results).sunset.toLocaleString('en-US', options);
         sunset.textContent = sunsetText;
